8d7748a4dcfe6f053ef1a8e3f4279cae7851321e
torikulislampatoary009 <torikulislampatoary@gmail.com>
2026-08-06 21:46:15 +0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ const loginError = document.getElementById("loginError");
 const loginButton = document.getElementById("loginButton");
 
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://yge-backend-production.up.railway.app:5000/api";
 
 
 loginForm.addEventListener("submit", async (event) => {
